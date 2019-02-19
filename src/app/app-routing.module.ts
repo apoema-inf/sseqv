551123dcb662625ddfddf99c4bed732f44371f77
Vinícius Alencar <viniciusale4@gmail.com>
2019-02-19 10:56:58 -0300

    navbar e slideshow

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,21 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [ {
+const routes: Routes = [{
   path: 'home',
-  component: HomeComponent }];
+  component: HomeComponent
+},
+
+{
+  path: '',
+  redirectTo: '/home',
+  pathMatch: 'full'
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
