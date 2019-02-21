547138c4a201433111cb2027496d49ba1dd2d259
Hyago Souza <hyagosouzza@hotmail.com>
2019-02-21 10:08:13 -0300

    alterações eliomar manha 21/02

--- src/app/sobre/sobre.component.ts
+++ src/app/sobre/sobre.component.ts
@@ -14,73 +14,87 @@ export class SobreComponent implements OnInit {
     this.coordenadores.push({
       nome: "Prof. Dr. Eliomar Araújo de Lima",
       cargo: "Coordenador Técnico",
-      lattes: "http://lattes.cnpq.br/1362170231777201"
+      lattes: "http://lattes.cnpq.br/1362170231777201",
+      avatar: "assets/img/eliomar.png"
     },
       {
         nome: "Prof. Dr. Fábio Nogueira de Lucena",
         cargo: "Subcoordenador Técnico",
-        lattes: "http://lattes.cnpq.br/6428011745982173"
+        lattes: "http://lattes.cnpq.br/6428011745982173",
+        avatar: "assets/img/fabio.png"
       })
 
     this.bolsistas.push({
       nome: "Carlos Hernane Oliveira",
       cargo: "Graduado em Sistemas de Informação",
-      lattes: ""
+      lattes: "http://lattes.cnpq.br/4808316415791351",
+      avatar: "assets/img/hernane.png"
     },
       {
         nome: "Douglas Tonetto Pfeifer",
-        cargo: "Graduando em Ciências da Computação",
-        lattes: ""
+        cargo: "Graduando em Ciência da Computação",
+        lattes: "",
+        avatar: "assets/img/douglas.jpeg"
       },
       {
         nome: "Hyago Vieira de Souza",
         cargo: "Graduando em Engenharia de Software",
-        lattes: ""
+        lattes: "http://lattes.cnpq.br/1023258037593936",
+        avatar: "assets/img/hyago.jpg"
       },
       {
         nome: "Karine Pires de Araújo",
-        cargo: "Graduanda em Ciências da Computação",
-        lattes: ""
+        cargo: "Graduanda em Ciência da Computação",
+        lattes: "http://lattes.cnpq.br/8660012741877683",
+        avatar: "assets/img/karine.jpeg"
       },
       {
         nome: "Luísa Nunes C. de Oliveira",
         cargo: "Graduanda em Gestão da Informação",
-        lattes: ""
+        lattes: "",
+        avatar: "assets/img/luisa.png"
       },
       {
         nome: "Max Stival Rocha",
         cargo: "Graduando em Sistemas de Informação",
-        lattes: ""
+        lattes: "",
+        avatar: "assets/img/max.jpeg"
       },
       {
         nome: "Nicolas de Sousa E. Da Cruz",
         cargo: "Graduando em Gestão da Informação",
-        lattes: ""
+        lattes: "",
+        avatar: "assets/img/nicolas.jpg"
       },
       {
         nome: "Raquel de Andrade Corrêa",
         cargo: "Graduanda em Sistemas de Informação",
-        lattes: ""
+        lattes: "",
+        avatar: "assets/img/raquel.jpeg"
       },
       {
         nome: "Thaynara Mabille Marques Ribeiro",
-        cargo: "Graduanda em Ciências da Computação",
-        lattes: ""
+        cargo: "Graduanda em Ciência da Computação",
+        lattes: "http://lattes.cnpq.br/4546159648766726",
+        avatar: "assets/img/thaynara.jpeg"
       },
       {
         nome: "Túlio Alcântara Magalhães",
-        cargo: "Graduando em Ciências da Computação",
-        lattes: ""
+        cargo: "Graduando em Ciência da Computação",
+        lattes: "",
+        avatar: "assets/img/tulio.png"
       },
       {
         nome: "Vinícius Borges Alencar",
-        cargo: "Graduando em Ciências da Computação",
-        lattes: ""
+        cargo: "Graduando em Ciência da Computação",
+        lattes: "",
+        avatar: "assets/img/vinicius.jpg"
       },
       {
         nome: "Ygor Galvão Lourenço",
         cargo: "Graduado em Gestão da Informação",
-        lattes: ""
+        lattes: "",
+        avatar: "assets/img/ygor.png"
       })
   }
 
