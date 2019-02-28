2fa1b8ffffc928e9af0bd39274990470d41dc1c6
vncsalencar <viniciusale4@gmail.com>
2019-02-28 18:08:19 -0300

    publicacao mudada

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
@@ -13,17 +13,17 @@ export class HomeComponent implements OnInit {
     constructor() {
 
         this.noticias.push({
-            titulo:'Health-Related Quality of Life and Well-Being',
+            titulo: 'Health-Related Quality of Life and Well-Being',
             descricao: 'Health-related quality of life (HRQoL) is a multi-dimensional concept that includes domains related to physical, mental, emotional, and social functioning.',
             link: 'https://www.healthypeople.gov/2020/about/foundation-health-measures/Health-Related-Quality-of-Life-and-Well-Being',
             imagem: ''
         });
         this.publicacoes.push({
-            titulo: 'Documentação SSEQV',
-            descricao: 'Sinopse do Projeto SSEQV',
-            label: 'PDF',
-            link: 'assets/anexos/Sinopse_ProjetoSSEQV.pdf'
-        });
+                titulo: 'Download',
+                descricao: 'Sinopse - SSEQV',
+                label: 'PDF',
+                link: 'assets/anexos/Sinopse_ProjetoSSEQV.pdf'
+            });
 
     }
 
